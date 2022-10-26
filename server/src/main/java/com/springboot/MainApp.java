482faf9cadc1cb375c15a2de0035cce097cb80a0
leo <leoxie838@qq.com>
2022-10-26 16:01:49 +0800

    fix: add java api backend

---
+++ server/src/main/java/com/springboot/MainApp.java
@@ -0,0 +1,21 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hk comp 2003 HongKongNatureWatch group pro
+ * @author Leo
+ * @date 24/10/2022
+ * @apiNote backend for the application front-end
+ * 
+ * */
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+	
+}
