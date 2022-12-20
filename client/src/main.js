33d3951f5dbaa7dc5013996ca4073710c54bbb89
ricky11 <rishi@pmgrouphk.com>
2022-12-20 20:14:22 +0800

    Added pdf of tree listings

--- client/src/main.js
+++ client/src/main.js
@@ -8,11 +8,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon as fa } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faBars,faTree,faXmark,faFilePen,faUser,faSpinner,faHouse,faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
+import { faBars,faTree,faXmark,faFilePen,faUser,faSpinner,faHouse,faCircleQuestion,faChartPie } from '@fortawesome/free-solid-svg-icons'
 import VueAnimXyz from '@animxyz/vue3'
 
 /* add icons to the library */
-library.add(faBars,faTree,faXmark,faFilePen,faUser,faSpinner,faHouse,faCircleQuestion)
+library.add(faBars,faTree,faXmark,faFilePen,faUser,faSpinner,faHouse,faCircleQuestion,faChartPie)
 
 import Vue3EasyDataTable from 'vue3-easy-data-table';
 import 'vue3-easy-data-table/dist/style.css';
