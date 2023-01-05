d78f32b42fbe457c345b4bfa17d403854146da7f
ricky11 <rishi@pmgrouphk.com>
2023-01-05 23:09:00 +0800

    adding google maps api key

--- client/src/components/DashNewSurvey.vue
+++ client/src/components/DashNewSurvey.vue
@@ -41,6 +41,20 @@
 
     <div>
       <h3 class="dark">Map Info</h3>
+      <div>
+
+        <iframe
+  width="600"
+  height="450"
+  style="border:0"
+  loading="lazy"
+  allowfullscreen
+  referrerpolicy="no-referrer-when-downgrade"
+  src="https://www.google.com/maps/embed/v1/place?key={{GOOGLE_API}}
+    &q=Space+Needle,Seattle+WA">
+</iframe>
+
+      </div>
     </div>
 
  
