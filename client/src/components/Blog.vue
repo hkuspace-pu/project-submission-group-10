c681705a928d52d1781265f71a2a4ef477a841f3
ricky11 <rishi@pmgrouphk.com>
2023-01-10 21:07:24 +0800

    Updated

--- client/src/components/Blog.vue
+++ client/src/components/Blog.vue
@@ -6,18 +6,5 @@
 
 
 <script setup>
-//   import { useStoryblokApi } from "@storyblok/vue";
-//   const storyblokApi = useStoryblokApi();
 
-//   const {data} = await storyblokApi.post("cdn/stories", {"starts_with": "faqs"});
-
-// const stories = await storyblokApi.get('spaces/hktreewatch/stories', {
-//     story : {
-//         name: "test",
-//         slug: "test",
-//         content : {
-//             component: 
-//         }
-//     }
-// })
 </script>
