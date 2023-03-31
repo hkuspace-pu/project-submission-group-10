500bd3a959b9a57853893beed7dda519bf3c6dd3
SimHo <howong7@gmail.com>
2023-03-31 11:34:32 +0800

    import d3 lib for tsv file upload

--- client/src/components/DashSyncFile.vue
+++ client/src/components/DashSyncFile.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="file-upload">
         <div class="file-upload__area">
-            <input type="file" name="" id="" />
+            <input type="file" name="" id="" @change="handleFileUpload" />
+        </div>
+
+        <div class="file-upload__area">
+            <table>
+                <tr v-for="( dRow, dIndex ) in tsvData" :key="dIndex" >
+                    <td v-for="( tRow, tIndex ) in tsvTitle" :key="tIndex">
+                        {{ dRow[tRow] }}
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
 </template>
@@ -10,8 +20,33 @@
 
 </script>
 <script>
+    import * as d3 from 'd3'
+
     export default {
         name: "FileUpload",
+        data() {
+            return {
+                tsvData: [],
+                tsvTitle: []
+            }
+        },
+        methods: {
+            handleFileUpload(ev) {
+                const file = ev.target.files[0]
+                if ( file ) {
+                    const reader = new FileReader()
+
+                    reader.onload = () => {
+                        const data  = reader.result
+
+                        var _res = d3.tsvParse(data)
+                        this.tsvData = _res
+                        this.tsvTitle = _res.columns
+                    }
+                    reader.readAsText(file)
+                }
+            }
+        },
     };
 </script>
 
